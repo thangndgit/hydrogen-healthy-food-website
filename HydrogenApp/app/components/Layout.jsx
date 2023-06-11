7d62e74f6b2e5a5ed8d4641d0077d19c1e3e6239
thangndgit <81653169+thangndgit@users.noreply.github.com>
2023-06-11 17:02:43 +0700

    Add app config, Login and Register UI

---
+++ HydrogenApp/app/components/Layout.jsx
@@ -0,0 +1,18 @@
+import Header from './Header';
+
+export function Layout({children, title}) {
+  return (
+    <div className="flex flex-col min-h-screen antialiased bg-neutral-50">
+      <Header title={title} />
+
+      <main
+        role="main"
+        id="mainContent"
+        // className="flex-grow p-6 md:p-8 lg:p-12"
+        className="flex-grow "
+      >
+        {children}
+      </main>
+    </div>
+  );
+}
---
+++ HydrogenApp/app/root.jsx
@@ -0,0 +1,69 @@
+import {
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  useLoaderData,
+} from '@remix-run/react';
+import tailwind from './styles/tailwind-build.css';
+import styles from './styles/app.css';
+import favicon from '../public/favicon.svg';
+import {Layout} from './components/Layout';
+import {Seo} from '@shopify/hydrogen';
+
+export const links = () => {
+  return [
+    {rel: 'stylesheet', href: tailwind},
+    {rel: 'stylesheet', href: styles},
+    {
+      rel: 'preconnect',
+      href: 'https://cdn.shopify.com',
+    },
+    {
+      rel: 'preconnect',
+      href: 'https://shop.app',
+    },
+    {rel: 'icon', type: 'image/svg+xml', href: favicon},
+  ];
+};
+
+export async function loader({context}) {
+  const layout = await context.storefront.query(LAYOUT_QUERY);
+  return {layout};
+}
+
+export default function App() {
+  const data = useLoaderData();
+
+  const {name} = data.layout.shop;
+
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Seo />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <div id="root-layout"></div>
+        <Layout title={name}>
+          <Outlet />
+        </Layout>
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
+
+const LAYOUT_QUERY = `#graphql
+  query layout {
+    shop {
+      name
+      description
+    }
+  }
+`;
